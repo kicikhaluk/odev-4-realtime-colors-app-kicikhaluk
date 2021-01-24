0ef27564f2c714a4466f2c841eff98f00bf10b05
kicikhaluk <f.halukkicik@outlook.com>
2021-01-24 18:58:27 +0300

    client linked with backend

--- client/src/services.js
+++ client/src/services.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 let socket;
 
 export const initSocket = () => {
-  socket = io("http://localhost:4000", {
+  socket = io("https://kodluyoruz-hfk.herokuapp.com", {
     transports: ["websocket"]
   });
   console.log("Connecting...");
